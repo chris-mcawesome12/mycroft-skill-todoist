661b15be114f6cee99886c380fa78969c0f41a8b
chris-mcawesome12 <chrismccann12@yahoo.ca>
2017-11-02 08:57:05 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -11,14 +11,16 @@ __author__ = 'chrismcawesome'
 
 LOGGER = getLogger(__name__)
 
-inbox = 151939893
-personal = 151939895
-shopping = 151939896
-work = 151939897
-errands = 151939898
-movies = 151939899
+##inbox = 151939893
+##personal = 151939895
+##shopping = 151939896
+##work = 151939897
+##errands = 151939898
+##movies = 151939899
+##homeassistant = 2166275738
+##mycroft = 2166275728
 
-api = TodoistAPI('YOUR AIP KEY')
+api = TodoistAPI('YOUR API KEY')
 
 class TodoistSkill1(MycroftSkill):
 
@@ -26,29 +28,38 @@ class TodoistSkill1(MycroftSkill):
         super(TodoistSkill1, self).__init__(name="TodoistSkill1")
 		
 	def initialize(self):
-            task_intent = IntentBuilder("TaskIntent").\
-                require("TaskKeyword").require("Task").require("Project").build()
-            self.register_intent(task_intent, self.handle_task_intent)
+        task_intent = IntentBuilder("TaskIntent").\
+            require("TaskKeyword").build()
+        self.register_intent(task_intent, self.handle_task_intent)
+		
+        shopping_intent = IntentBuilder("ShoppingIntent").\
+            require("ShoppingKeyword").build()
+        self.register_intent(shopping_intent, self.handle_shopping_intent)
 
-        def handle_task_intent(self, message):
-            task = message.data["Task"]
-            project = message.data["Project"]
-            if project == "personal":
-                project = personal
-                api.sync()
-                Task1 = api.items.add(task, project)
-                api.commit()
-                self.speak("task added")
+        movie_intent = IntentBuilder("MovieIntent").\
+            require("MovieKeyword").build()
+        self.register_intent(movie_intent, self.handle_movie_intent)
+		
+    def handle_task_intent(self, message):
+		api.sync()
+		taks1 = api.items.add('Task1', 151939895)
+		api.commit()
+        self.speak("task added to to do list")
 
-            else:
-                project = shopping
-                api.sync()
-                Task1 = api.items.add(task, project)
-                api.commit()
-                self.speak("task added")
+    def handle_shopping_intent(self, message):
+		api.sync()
+		taks1 = api.items.add('Task1', 151939896)
+		api.commit()
+        self.speak("item added to shopping list")
 
+    def handle_movie_intent(self, message):
+		api.sync()
+		taks1 = api.items.add('Task1', 151939899)
+		api.commit()
+        self.speak("movie added to to list")
+		
 	def stop(self):
-            pass
+        pass
 
 def create_skill():
     return TodoistSkill1()
